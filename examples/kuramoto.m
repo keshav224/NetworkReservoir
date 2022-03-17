4440c9b002b3771410be0b32e4ecc2656bddb595
Keshav Srinivasan <keshavsrinivasan224@gmail.com>
2022-03-17 06:02:54 -0400

    Small Fixes to Initial Commit

--- examples/kuramoto.m
+++ examples/kuramoto.m
@@ -1,5 +1,6 @@
 addpath('../code');
 load('../data/kuramoto_50.mat');
+data = reshape(datfile,resparams.NetworkSize,[],resparams.dimension);
 [prediction,truth] = parallel_run(data, A, resparams);
 
 plotIndices = [2,41,17];
